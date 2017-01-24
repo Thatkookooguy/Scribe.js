513012b508558823e2395c59e5d32baf8c572172
Neil Kalman <Neilkalman@gmail.com>
2017-01-24 16:58:49 +0200

    don't remove the url prefix

--- static/js/filters/trust.js
+++ static/js/filters/trust.js
@@ -8,12 +8,12 @@
           var numberOfVars = Object.keys(log.args).length;
           for(var i = 0; i < numberOfVars; i++) {
             completeString += i === 0 ?
-              Autolinker.link( ansi_up.ansi_to_html(log.args[i]) ) + '\n' :
+              Autolinker.link( ansi_up.ansi_to_html(log.args[i]), { stripPrefix: false } ) + '\n' :
               Autolinker.link( [
                 '<span class="hljs">',
                 hljs.highlight('json', JSON.stringify(log.args[i], undefined, 2), true, false).value,
                 '</span>\n'
-              ].join('') );
+              ].join(''), { stripPrefix: false } );
           }
             return $sce.trustAsHtml(completeString);
         }
